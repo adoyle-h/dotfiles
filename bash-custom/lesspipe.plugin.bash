08fcba920ef056071d6d4a54ce64fa398c1bfb7f
ADoyle <adoyle.h@gmail.com>
2017-02-02 20:13:45 +0800

    update(bash-custom): lesspipe

--- bash-custom/lesspipe.plugin.bash
+++ bash-custom/lesspipe.plugin.bash
@@ -1,2 +1,14 @@
+# Depend on source-highlight. To install: brew install source-highlight
+# READ: http://liyangliang.me/posts/2013/11/less-with-syntax-highlight-and-line-number/
 # make less more friendly for non-text input files, see lesspipe(1)
-[[ -x /usr/bin/lesspipe ]] && eval "$(SHELL=/bin/sh lesspipe)"
+if [[ -x /usr/bin/lesspipe ]]; then
+  LESSPIPE_SCRIPT=/usr/bin/lesspipe
+  eval "$(SHELL=/bin/sh lesspipe)"
+elif [[ -x /usr/local/bin/src-hilite-lesspipe.sh ]]; then
+  LESSPIPE_SCRIPT=/usr/local/bin/src-hilite-lesspipe.sh
+fi
+
+if [[ -n "$LESSPIPE_SCRIPT" ]]; then
+  export LESSOPEN="| ${LESSPIPE_SCRIPT} %s"
+  export LESS=" -R "
+fi

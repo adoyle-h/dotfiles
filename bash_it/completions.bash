e2e575dafe5d135023001c980e888f7a13ed2d84
ADoyle <adoyle.h@gmail.com>
2017-02-04 20:42:39 +0800

    fix(completion): only enable bash-completion2 for bash v4

--- bash_it/completions.bash
+++ bash_it/completions.bash
@@ -3,18 +3,21 @@
 
 # System completion should be first
 
+GET_BASH_MAJOR_VERSION() {
+  "$BASH" --version | sed -r -n 's@.*[(version)|(版本)] ([0-9]{1,})\.[0-9]{1,}\.[0-9]{1,}\(1\)-release.*@\1@p'
+}
+# echo GET_BASH_MAJOR_VERSION-----$GET_BASH_MAJOR_VERSION
+
 if [[ -x "$(command -v brew)" ]] ; then
   # If Homebrew is installed, its Bash completion modules are loaded.
   BREW_PREFIX=$(brew --prefix)
 
   if [[ -n "$BREW_PREFIX" ]]; then
     # To enable bash_completion in macos
-    BASH_COMPLETION_FILE="$BREW_PREFIX/share/bash-completion/bash_completion"
-
-    if [ -f "$BREW_PREFIX"/share/bash-completion/bash_completion ]; then
+    if [[ -f "$BREW_PREFIX"/share/bash-completion/bash_completion ]] && [[ "$(GET_BASH_MAJOR_VERSION)" == 4 ]]; then
       # bash-completion2
       BASH_COMPLETION_FILE="$BREW_PREFIX"/share/bash-completion/bash_completion
-    elif [ -f "$BREW_PREFIX"/etc/bash_completion ]; then
+    elif [[ -f "$BREW_PREFIX"/etc/bash_completion ]]; then
       # bash-completion
       BASH_COMPLETION_FILE="$BREW_PREFIX"/etc/bash_completion
     fi
@@ -33,5 +36,6 @@ else
 fi
 
 if [[ -n "$BASH_COMPLETION_FILE" ]] && [[ -f "$BASH_COMPLETION_FILE" ]]; then
+  # echo BASH_COMPLETION_FILE-----$BASH_COMPLETION_FILE
   source "$BASH_COMPLETION_FILE"
 fi

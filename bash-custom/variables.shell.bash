ef7286872412a063ae6d8faefad07cff1dc75288
ADoyle <adoyle.h@gmail.com>
2017-02-22 20:41:57 +0800

    update(shell): update HISTIGNORE

--- bash-custom/variables.shell.bash
+++ bash-custom/variables.shell.bash
@@ -1,7 +1,7 @@
 #---------------------------- Shell Variables --------------------------------#
 
 ## shell history
-HISTIGNORE='ls:sl:bg:fg:pwd:history'
+HISTIGNORE='ls:sl:pwd:history:clear'
 # don't put duplicate lines in the history. See bash(1) for more options
 HISTCONTROL=ignoredups
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)

6485c7fecdc6a1f5fa334be716697bed1519100a
ADoyle <adoyle.h@gmail.com>
2017-02-24 10:28:01 +0800

    add(plugin): rust

---
+++ bash-custom/available/rust.plugin.bash
@@ -0,0 +1,15 @@
+cite about-plugin
+about-plugin 'Rust pre-settings'
+
+CARGO_BIN=$HOME/.cargo/bin
+if [[ -d $CARGO_BIN ]] ; then
+  export PATH="$PATH:$CARGO_BIN"
+else
+  echo "Not found folder $CARGO_BIN" >&2
+  return 1
+fi
+
+# Enable auto completion
+if has command rustup; then
+  eval "$(rustup completions bash)"
+fi

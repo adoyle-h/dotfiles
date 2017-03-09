dd3c88ec7a676d1f471b752e477279991a2705a1
ADoyle <adoyle.h@gmail.com>
2017-03-09 22:36:28 +0800

    feat(tmux): enable mouse features

--- bash-custom/available/tmux.plugin.bash
+++ bash-custom/available/tmux.plugin.bash
@@ -3,4 +3,14 @@ about-plugin 'Tmux pre-settings'
 
 alias tm='tmux attach-session -t "work" || tmux new-session -s "work" -d -n "main"'
 
+function tmux-mouse-on() {
+  tmux set -g mouse on
+  tmux display-message 'Mouse: ON'
+}
+
+function tmux-mouse-off() {
+  tmux set -g mouse off
+  tmux display-message 'Mouse: Off'
+}
+
 [[ -z "$TMUX" ]] && export TMUX

f89a35ef67e1b31e6fcd703d7a5299a9c56c1c20
ADoyle <adoyle.h@gmail.com>
2018-02-01 21:40:36 +0800

    fix(bash-custom): env.shell.bash

--- bash-custom/env.shell.bash
+++ bash-custom/env.shell.bash
@@ -1,8 +1,9 @@
 #--------------------- Shell Environment Configuration -----------------------#
 
-if [[ $(echo $0) == '-bash' ]]; then
+if [[ "$0" == '-bash' ]]; then
 
 # Disable `alt-numkey` in bash shell. Refer to http://superuser.com/a/770902
+# Do NOT use explicit escaping: "\\e"
 for i in "-" "n" "p" "" {0..9}; do bind -r "\e$i"; done
 
 # https://www.gnu.org/software/grep/manual/html_node/Environment-Variables.html

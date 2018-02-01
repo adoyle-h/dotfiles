80e76caf1e66cb076b75b46fb41b64847057525c
ADoyle <adoyle.h@gmail.com>
2018-02-01 20:20:49 +0800

    fix: disable GREP_COLOR and alt-numkey modification while in non-login shell

--- bash-custom/env.shell.bash
+++ bash-custom/env.shell.bash
@@ -1,7 +1,11 @@
 #--------------------- Shell Environment Configuration -----------------------#
 
+if [[ $(echo $0) == '-bash' ]]; then
+
 # Disable `alt-numkey` in bash shell. Refer to http://superuser.com/a/770902
 for i in "-" "n" "p" "" {0..9}; do bind -r "\e$i"; done
 
 # https://www.gnu.org/software/grep/manual/html_node/Environment-Variables.html
 export GREP_COLOR='01;31'
+
+fi

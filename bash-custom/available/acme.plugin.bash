8cceb33093eb0f4e109f60c6e95ee3ee8e8e5bb4
ADoyle <adoyle.h@gmail.com>
2018-12-24 00:43:50 +0800

    fix(bash-custom): acme.plugin.bash

--- bash-custom/available/acme.plugin.bash
+++ bash-custom/available/acme.plugin.bash
@@ -2,7 +2,7 @@ cite about-plugin
 about-plugin 'Enable acme.sh'
 # https://github.com/Neilpang/acme.sh
 
-[[ ! -f "$HOME/.acme.sh/acme.sh.env" ]] return;
+[[ ! -f "$HOME/.acme.sh/acme.sh.env" ]] && return;
 
 source "$HOME/.acme.sh/acme.sh.env"
 alias acme.sh='~/.acme.sh/acme.sh'

11d740bd544c5254e3e5ecb1c818a40032bce99f
ADoyle <adoyle.h@gmail.com>
2019-02-20 23:54:19 +0800

    update: change _ZL_CMD z=>j

--- bash-custom/available/zl.plugin.bash
+++ bash-custom/available/zl.plugin.bash
@@ -5,7 +5,7 @@ if [[ ! -f $DOTFILE_DIR/pkgs/z.lua/z.lua ]]; then
   return 0
 fi
 
-export _ZL_CMD=z
+export _ZL_CMD=j
 export _ZL_DATA=$HOME/.z_lua
 # export _ZL_NO_PROMPT_COMMAND=true
 export _ZL_EXCLUDE_DIRS=(.git)
@@ -14,9 +14,9 @@ export _ZL_MATCH_MODE=1
 
 eval "$(lua "$DOTFILE_DIR"/pkgs/z.lua/z.lua --init bash)"
 
-alias zu='z -b'      # cd to the parent directory starting with foo
-alias zl='z -l foo'  # list matches instead of cd
-alias zc='z -c'      # restrict matches to subdirs of $PWD
-alias zz='z -i'      # cd with interactive selection
-alias zf='z -I'      # cd with interactive selection using fzf
-alias zb='z -t -I .' # go back to last directory
+alias ju='j -b'      # cd to the parent directory starting with foo
+alias jl='j -l foo'  # list matches instead of cd
+alias jc='j -c'      # restrict matches to subdirs of $PWD
+alias jz='j -i'      # cd with interactive selection
+alias jf='j -I'      # cd with interactive selection using fzf
+alias jb='j -t -I .' # go back to last directory

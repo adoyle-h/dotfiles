e6b52e3113d5c0bb77b3c54d55cd86e87921279d
ADoyle <adoyle.h@gmail.com>
2019-05-27 22:28:12 +0800

    update: change sub framework file structure

--- bash_it/plugins/available/sub.plugin.bash
+++ bash_it/plugins/available/sub.plugin.bash
@@ -4,5 +4,5 @@ about-plugin 'My "sub" command entry.'
 
 export SUB_NAME="a"
 export SUB_BIN_DIR="$HOME/bin/sub"
-export SUB_COMPLETION_FILE="$HOME/.bash_completions/others/sub.bash"
-eval "$("$SUB_NAME" init -)"
+export SUB_COMPLETION_FILE="$(dirname "${BASH_SOURCE[0]}")/../available/sub/sub.completion.bash"
+eval "$("$(dirname "${BASH_SOURCE[0]}")"/../available/sub/init)"

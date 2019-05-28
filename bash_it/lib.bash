7e0b24d58543aa39c650a6f6543d9ba89e8c67c3
ADoyle <adoyle.h@gmail.com>
2019-05-28 23:34:24 +0800

    fix(plugin/nvm): wrong warning log

--- bash_it/lib.bash
+++ bash_it/lib.bash
@@ -34,7 +34,7 @@ has() {
   local condition="$1"
   local value="$2"
 
-  if [ "$condition" == "not" ]; then
+  if [[ "$condition" == "not" ]]; then
     shift 1
     ! has "${@}"
     return $?
@@ -43,6 +43,18 @@ has() {
   case "$condition" in
     command)
       [[ -x "$(command -v "$value")" ]] && return 0;;
+    function)
+      [[ $(type -t "$value") == function ]] && return 0;;
+    alias)
+      [[ $(type -t "$value") == alias ]] && return 0;;
+    keyword)
+      [[ $(type -t "$value") == keyword ]] && return 0;;
+    builtin)
+      [[ $(type -t "$value") == builtin ]] && return 0;;
+    the)
+      type -t "$value"
+      return $?
+      ;;
   esac > /dev/null
 
   return 1
--- bash_it/plugins/available/nvm.plugin.bash
+++ bash_it/plugins/available/nvm.plugin.bash
@@ -7,22 +7,21 @@ export NODEJS_ORG_MIRROR="https://npm.taobao.org/mirrors/node"
 export NVM_NODEJS_ORG_MIRROR="$NODEJS_ORG_MIRROR"
 
 brew_nvm_prefix="$(brew --prefix nvm)"
-if command -v brew &>/dev/null && [[ -s $brew_nvm_prefix/nvm.sh ]]; then
-  export NVM_DIR=${NVM_DIR:-$HOME/.nvm}
+if has command brew && [[ -s $brew_nvm_prefix/nvm.sh ]]; then
+  export NVM_DIR=$brew_nvm_prefix
   . "$brew_nvm_prefix"/nvm.sh
-
   unset brew_nvm_prefix
-else
+elif [[ -s ${NVM_DIR:-$HOME/.nvm} ]]; then
   export NVM_DIR=${NVM_DIR:-$HOME/.nvm}
-  [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
+  . "$NVM_DIR/nvm.sh"
 fi
 
-if [ "$NVM_DIR" ] && [ -r "$NVM_DIR"/bash_completion ]; then
+if [[ -n "$NVM_DIR" ]] && [[ -r "$NVM_DIR"/bash_completion ]]; then
   . "$NVM_DIR"/bash_completion
 fi
 
-if has not command nvm; then
-  echo 'WARNING: Command nvm not found. But you have enabled nvm.plugin.bash.' >&2
+if has not function nvm; then
+  echo 'WARNING: Shell function "nvm" not found. But you have enabled nvm.plugin.bash.' >&2
   echo '         You should install nvm by yourself. See https://github.com/creationix/nvm' >&2
   echo '         If you install nvm via brew. Check the path of "brew --prefix nvm".' >&2
   echo "         If you install nvm via curl. Check the path of \"$NVM_DIR/nvm.sh\"." >&2

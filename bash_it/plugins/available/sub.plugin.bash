d0d4c12b3cd9d35a06b4a96b77947bebd1d0b2b0
ADoyle <adoyle.h@gmail.com>
2019-06-20 23:23:50 +0800

    fix: change plugins related commands && update custom_plugins

--- bash_it/plugins/available/sub.plugin.bash
+++ bash_it/plugins/available/sub.plugin.bash
@@ -2,6 +2,7 @@
 cite about-plugin
 about-plugin 'My "sub" command entry.'
 
+# Do not modify SUB_NAME!
 export SUB_NAME="a"
 export SUB_BIN_DIR="$HOME/bin/sub"
 SUB_COMPLETION_FILE="$(dirname "${BASH_SOURCE[0]}")/../available/sub/sub.completion.bash"

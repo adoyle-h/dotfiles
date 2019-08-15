520b99cfeb31a3adebb193fcb4313762c73b6633
ADoyle <adoyle.h@gmail.com>
2019-08-16 00:16:11 +0800

    add(plugin): basher

---
+++ bash_it/plugins/available/basher.plugin.bash
@@ -0,0 +1,13 @@
+cite about-plugin
+about-plugin 'Init basher. https://github.com/basherpm/basher'
+
+if dotfiles_l.has_not command basher; then
+  echo "Not found command 'basher" >&2
+  echo "WARNING: Invoke 'a disable-plugin basher' to disable the plugin" >&2
+  return 0
+fi
+
+BASHER_FULL_CLONE=false
+# BASHER_PREFIX=$HOME/.basher/cellar
+
+source <(basher init -)

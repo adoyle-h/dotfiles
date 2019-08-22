9c1b975dafe3e1508911e5ce39d9ce174d223058
ADoyle <adoyle.h@gmail.com>
2019-08-23 00:18:08 +0800

    remove: polipo

--- bash_it/plugins/available/proxy.plugin.bash
+++ bash_it/plugins/available/proxy.plugin.bash
@@ -6,7 +6,6 @@ export ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron/
 ## homebrew 镜像 https://lug.ustc.edu.cn/wiki/mirrors/help/homebrew-bottles
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
-# export BASH_IT_HTTP_PROXY='http://127.0.0.1:28124' # polipo port
 export BASH_IT_HTTP_PROXY='socks5://127.0.0.1:1080' # shadowsocks port
 export BASH_IT_HTTPS_PROXY=$BASH_IT_HTTP_PROXY
 export BASH_IT_NO_PROXY='127.0.0.1,localhost'

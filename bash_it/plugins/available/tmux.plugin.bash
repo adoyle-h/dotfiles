3ab874b6a8cf67b3dc0cdaafbd0111963222d310
ADoyle <adoyle.h@gmail.com>
2019-09-03 02:52:40 +0800

    update(tmux): add new tmux plugins && new status line && update tmux config

--- bash_it/plugins/available/tmux.plugin.bash
+++ bash_it/plugins/available/tmux.plugin.bash
@@ -3,17 +3,29 @@ about-plugin 'Tmux pre-settings'
 
 alias tm='tmux attach-session -t "work" || tmux new-session -s "work" -d -n "main"'
 
-function tmux-mouse-on() {
+function tmux_mouse_turn_on() {
   tmux set -g mouse on
   tmux display-message 'Mouse: ON'
 }
 
-function tmux-mouse-off() {
+function tmux_mouse_turn_off() {
   tmux set -g mouse off
   tmux display-message 'Mouse: Off'
 }
 
-function tmux-list-panes() {
+function tmux_toggle_mouse() {
+  local old
+  old=$(tmux show -gv mouse)
+  local new=""
+
+  if [ "$old" = "on" ]; then
+    tmux_mouse_turn_off
+  else
+    tmux_mouse_turn_on
+  fi
+}
+
+function tmux_list_panes() {
   tmux lsp -F '#{pane_index}: [#{pane_width}x#{pane_height}] paneId=#{pane_id} #{pane_tty} #{?pane_active,(active),} #{?pane_dead,(dead),}'
 }
 

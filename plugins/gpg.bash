b292bbe61c77f033ad21827e3e9e2bcb57b63eb4
ADoyle <adoyle.h@gmail.com>
2019-12-01 18:48:52 +0800

    fix: gpg configuration

--- plugins/gpg.bash
+++ plugins/gpg.bash
@@ -1,5 +1,9 @@
+# BASH_IT_LOAD_PRIORITY: 900
 cite about-plugin
 about-plugin 'GPG settings'
 
-GPG_TTY=$(tty)
+# https://wiki.archlinux.org/index.php/GnuPG#Configure_pinentry_to_use_the_correct_TTY
+# pinentry need GPG_TTY to launch. Otherwise GPG sign will fail.
 export GPG_TTY
+# @TODO Why it always return 'not a tty'?
+GPG_TTY=$(tty)

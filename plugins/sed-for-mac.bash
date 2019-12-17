bdeff96a40a67e0bc9b65a54c9648c2839ed1321
ADoyle <adoyle.h@gmail.com>
2019-12-17 16:50:11 +0800

    fix(plugin/sed-for-mac): gnu-sed should not instead of default sed in MacOS. You can use gsed.

--- plugins/sed-for-mac.bash
+++ plugins/sed-for-mac.bash
@@ -2,7 +2,7 @@
 cite about-plugin
 about-plugin 'Use gnu-sed instead of default sed for MacOS'
 
-gnu_sed_bin=/usr/local/opt/gnu-sed/libexec/gnubin
+gnu_sed_bin=/usr/local/opt/gnu-sed/bin
 if [[ -d $gnu_sed_bin ]] && ( ! dotfiles_l.str_include "$PATH" "$gnu_sed_bin" ); then
   export PATH="$gnu_sed_bin:$PATH"
   export MANPATH="$MANPATH:/usr/local/opt/gnu-sed/libexec/gnuman"

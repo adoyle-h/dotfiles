9984e0f9a5973ee652741850776d645fdc09732a
ADoyle <adoyle.h@gmail.com>
2020-03-18 00:14:26 +0800

    update submodules

--- plugins/prompt.bash
+++ plugins/prompt.bash
@@ -2,5 +2,6 @@ cite about-plugin
 about-plugin 'My pretty bash prompt instead of bash-it theme.'
 
 PROMPT_ENABLE_HISTORY_APPEND=1
+PROMPT_PYTHON_VIRTUALENV_LEFT=
 # shellcheck source=../deps/a-bash-prompt/a.prompt.bash
 source "$DOTFILES_DIR/deps/a-bash-prompt/a.prompt.bash"

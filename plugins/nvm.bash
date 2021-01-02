276fec3f0547908e3e4587d15dc4d9de37d8cf95
ADoyle <adoyle.h@gmail.com>
2021-01-02 23:52:16 +0800

    chore(nvm): remove comment

--- plugins/nvm.bash
+++ plugins/nvm.bash
@@ -2,7 +2,6 @@
 cite about-plugin
 about-plugin 'change node source code mirror and enable nvm'
 
-# NVM_NODEJS_ORG_MIRROR is deprecated and will be removed in node-gyp v4, please use NODEJS_ORG_MIRROR
 export NODEJS_ORG_MIRROR="https://npm.taobao.org/mirrors/node"
 export NVM_NODEJS_ORG_MIRROR="$NODEJS_ORG_MIRROR"
 

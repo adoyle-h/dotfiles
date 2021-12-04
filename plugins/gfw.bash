ab0a6799b277926c6d68b73b4a8b529c9f935fa2
ADoyle <adoyle.h@gmail.com>
2021-12-04 20:43:31 +0800

    fix(plugin): comment homebrew variables in gfw plugin

--- plugins/gfw.bash
+++ plugins/gfw.bash
@@ -3,8 +3,10 @@ about-plugin 'Settings for GFW'
 
 export ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron/
 
-## homebrew 镜像 https://lug.ustc.edu.cn/wiki/mirrors/help/homebrew-bottles
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+# # https://mirrors.tuna.tsinghua.edu.cn/help/homebrew/
+# export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+# export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+# export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 
 export BASH_IT_HTTP_PROXY='socks5://127.0.0.1:1080' # shadowsocks port
 export BASH_IT_HTTPS_PROXY=$BASH_IT_HTTP_PROXY

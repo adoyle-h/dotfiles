0b686b33306492e78c9070a34197f16b62c3f78f
ADoyle <adoyle.h@gmail.com>
2021-12-15 00:36:39 +0800

    fix(plugins/java): set CPPFLAGS && check files existed first

--- plugins/java.bash
+++ plugins/java.bash
@@ -2,8 +2,14 @@ cite about-plugin
 about-plugin 'Java settings'
 
 if [[ "$(uname -s)" == "Darwin" ]] ; then
-  # https://mkyong.com/java/how-to-set-java_home-environment-variable-on-mac-os-x/
-  export JAVA_HOME=$(/usr/libexec/java_home)
+  if [[ -d /usr/libexec/java_home ]]; then
+    # https://mkyong.com/java/how-to-set-java_home-environment-variable-on-mac-os-x/
+    export JAVA_HOME=$(/usr/libexec/java_home)
+  fi
+
+  if [[ -d $HOMEBREW_PREFIX/opt/openjdk/include ]]; then
+    export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/openjdk/include"
+  fi
 else
   # @TODO for Windows and Linux system
   true

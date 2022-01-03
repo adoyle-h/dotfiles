4e630f37dd82c2074e9c0033910cc504418575bb
ADoyle <adoyle.h@gmail.com>
2022-01-03 23:25:54 +0800

    feat: add plugin and settings for linux system

--- bash/os-settings.bash
+++ bash/os-settings.bash
@@ -5,6 +5,9 @@ case $(dotfiles_l.detect_os) in
     # shellcheck source=./macos.bash
     source "$DOTFILES_DIR/bash/macos.bash"
     ;;
+  Linux)
+    source "$DOTFILES_DIR/bash/linux.bash"
+    ;;
   *)
     ;;
 esac

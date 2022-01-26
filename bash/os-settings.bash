2b1b5ec63e1400689abf2aa0b3e6a09231456f1f
ADoyle <adoyle.h@gmail.com>
2022-01-27 01:29:03 +0800

    feat(bash): add new environment variable DOTFILES_OS

--- bash/os-settings.bash
+++ bash/os-settings.bash
@@ -1,6 +1,8 @@
 # Required variables which OS related for plugins should put in this file.
 
-case $(dotfiles_l.detect_os) in
+DOTFILES_OS=$(dotfiles_l.detect_os)
+
+case $DOTFILES_OS in
   MacOS)
     # shellcheck source=./macos.bash
     source "$DOTFILES_DIR/bash/macos.bash"

d12ccc206da9769dff433280c967f0c3fcddbcc4
ADoyle <adoyle.h@gmail.com>
2022-01-31 03:53:21 +0800

    update(plugins/ls): show create time and access time both

--- plugins/ls.bash
+++ plugins/ls.bash
@@ -3,8 +3,8 @@ about-plugin 'better ls commands. use https://github.com/ogham/exa'
 
 if dotfiles_l.has command exa; then
   alias ls='exa -x'
-  alias ll='exa -lahgb --icons --time-style=long-iso'
-  alias l='exa -lahgb --icons --time-style=long-iso'
+  alias ll='exa -lahgbUu --icons --time-style=long-iso'
+  alias l='exa -lahgbUu --icons --time-style=long-iso'
   alias llm='ll -s modified'
 else
   if ls --color -d . &> /dev/null ; then

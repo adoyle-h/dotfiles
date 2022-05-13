dc8f00a582b04de047edf3d966415fa206ac36b4
ADoyle <adoyle.h@gmail.com>
2022-05-13 14:03:52 +0800

    update: move plugins/rm to aliases/avoid-mistakes

--- aliases/avoid-mistakes.bash
+++ aliases/avoid-mistakes.bash
@@ -1,6 +1,12 @@
 cite about-plugin
 about-plugin 'Some more alias to avoid making mistakes'
 
-alias rm='rm -i'
+if dotfiles_l.has command trash; then
+  # Safe "rm" command. Use trash if it exists. https://github.com/sindresorhus/trash
+  alias rm='trash'
+else
+  alias rm='rm -i'
+fi
+
 alias cp='cp -i'
 alias mv='mv -i'
--- plugins/rm.bash
+++
@@ -1,8 +0,0 @@
-cite about-plugin
-about-plugin 'Safe "rm" command. Use trash if it exists. https://github.com/sindresorhus/trash'
-
-if dotfiles_l.has command trash; then
-  alias rm='trash'
-else
-  alias rm='rm -i'
-fi

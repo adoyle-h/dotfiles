1dd6c26f534ed1dffba3b1ad33bf0be4f6a39dee
ADoyle <adoyle.h@gmail.com>
2022-10-13 20:58:33 +0800

    fix(plugin): update welcome text

--- plugins/welcome.bash
+++ plugins/welcome.bash
@@ -5,12 +5,12 @@ about-plugin 'print welcome message on first login shell'
 cat <<EOF
 ${BLUE_ESC}
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-                         Welcome! ${USER^^}
                              　/\\/\\
                              =(°w°)=
                              　)❣(⎛
                               (___)
-                         Enjoy Your Life!
+                         Welcome! ${USER^^}
+                     Enjoy Coding, Enjoy Life!
 
 $(w | awk '{print "  " $0}')
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
